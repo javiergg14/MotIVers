bae609430c3cfdf722d72d6c88b76d0c334d3678
TuMamaMeGusta <javigg011205@gmail.com>
2024-10-06 18:44:12 +0200

    Options

--- PremisLudi/Assets/GameTransition.cs
+++ PremisLudi/Assets/GameTransition.cs
@@ -5,19 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class GameTransition : MonoBehaviour
 {
+    public GameObject panelToShow; // Panel que se mostrará
+    public GameObject panelToHide; // Panel que se ocultará
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Asegúrate de que ambos paneles estén en el estado inicial deseado
+        if (panelToShow != null)
+        {
+            panelToShow.SetActive(false); // Desactivar el panel a mostrar inicialmente
+        }
+
+        if (panelToHide != null)
+        {
+            panelToHide.SetActive(true); // Activar el panel a ocultar inicialmente
+        }
     }
 
-    // Update is called once per frame
+    // Update is called once por frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
             SceneManager.LoadScene("Game");
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Alternar entre los paneles al presionar Escape
+            if (panelToShow != null && panelToHide != null)
+            {
+                if (panelToShow.activeSelf)
+                {
+                    // Si el panel a mostrar está activo, ocúltalo y muestra el panel a ocultar
+                    panelToShow.SetActive(false); // Ocultar el panel a mostrar
+                    panelToHide.SetActive(true); // Mostrar el panel a ocultar
+                }
+                else
+                {
+                    // Si el panel a ocultar está activo, ocúltalo y muestra el panel a mostrar
+                    panelToHide.SetActive(false); // Ocultar el panel a ocultar
+                    panelToShow.SetActive(true); // Mostrar el panel a mostrar
+                }
+            }
+        }
     }
 
+    // Método que se llamará desde el botón del nuevo panel
+    public void ReturnToOriginalPanel()
+    {
+        if (panelToShow != null && panelToHide != null)
+        {
+            panelToShow.SetActive(false); // Ocultar el panel a mostrar
+            panelToHide.SetActive(true); // Mostrar el panel a ocultar
+        }
+    }
 }
