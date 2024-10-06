96b72abe5c1238da5ea5b6ecc63f7b629a76dce8
TuMamaMeGusta <javigg011205@gmail.com>
2024-10-06 12:35:42 +0200

    Renombrar algunos assets, cambio de tamaño camara, arboles, pantalla de inicio, y mejora transicion animaciones

--- PremisLudi/Assets/PlayerConfg.cs
+++ PremisLudi/Assets/PlayerConfg.cs
@@ -2,33 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxController : MonoBehaviour // Cambia el nombre de la clase
+public class Box : MonoBehaviour
 {
-    public float speed = 5f;
-    public Animator animator;
-
-    public float maxTiltAngle = 10f; // Ángulo máximo permitido antes de "doblarse"
-
-   
+    public float speed = 5f; // Velocidad de movimiento
+    public Animator animator; // Referencia al componente Animator
 
     void Update()
     {
-        // Movimiento
+        // Obtener el input de los ejes horizontal y vertical
         float speedX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float speedY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
+        // Configurar el parámetro de animación "movement" según el valor de la velocidad horizontal
         animator.SetFloat("movementX", speedX * speed);
         animator.SetFloat("movementY", speedY * speed);
 
-        // Actualiza la posición del objeto
+
+        // Obtener la posición actual
         Vector3 posicion = transform.position;
+
+        // Actualizar la posición en los ejes X y Y (movimiento horizontal y vertical)
         transform.position = new Vector3(speedX + posicion.x, speedY + posicion.y, posicion.z);
     }
-
- 
 }
-
-
-
 
 
