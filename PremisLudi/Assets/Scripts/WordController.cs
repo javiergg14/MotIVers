f73214f4766d12091cf13555e19c2ade55d5e5a7
oscaralonsoo <alonsocamenforte@gmail.com>
2024-10-13 20:59:18 +0200

    Change class name WordController

--- PremisLudi/Assets/Scripts/WordController.cs
+++ PremisLudi/Assets/Scripts/WordController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Word : MonoBehaviour
+public class WordController : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
